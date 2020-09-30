24ede40a76419e2f5a92f22dec35e4fb00febd69
Patrick James <pj997067@gmail.com>
2020-09-30 17:53:04 -0400

    Remove crossdatabase test

--- tests/AutoPocoIO.MsSql.Shared.test/DynamicSchema/Services/CrossdbTests.cs
+++ tests/AutoPocoIO.MsSql.Shared.test/DynamicSchema/Services/CrossdbTests.cs
@@ -12,20 +12,20 @@ namespace AutoPocoIO.MsSql.DynamicSchema.Services
     [TestCategory(TestCategories.Unit)]
     public class CrossdbTests
     {
-        [TestMethod]
-        public void MyTestMethod()
-        {
-            DbContextOptions<AppDbContext> AppDbOptions = (DbContextOptions < AppDbContext > ) new DbContextOptionsBuilder<AppDbContext>()
-               .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=autopocoCore;Integrated Security=True;MultipleActiveResultSets=True;App=EntityFramework")
-               .ReplaceEFCrossDbServices()
-               .ReplaceSqlServerEFCrossDbServices()
-               .ReplaceEFCachingServices()
-               .ReplaceSqlServerEFCachingServices()
-               .Options;
+        //[TestMethod]
+        //public void MyTestMethod()
+        //{
+        //    DbContextOptions<AppDbContext> AppDbOptions = (DbContextOptions < AppDbContext > ) new DbContextOptionsBuilder<AppDbContext>()
+        //       .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=autopocoCore;Integrated Security=True;MultipleActiveResultSets=True;App=EntityFramework")
+        //       .ReplaceEFCrossDbServices()
+        //       .ReplaceSqlServerEFCrossDbServices()
+        //       .ReplaceEFCachingServices()
+        //       .ReplaceSqlServerEFCachingServices()
+        //       .Options;
 
-            var context = new AppDbContext(AppDbOptions);
+        //    var context = new AppDbContext(AppDbOptions);
 
-            var a = context.Connector.ToList();
-        }
+        //    var a = context.Connector.ToList();
+        //}
     }
 }
